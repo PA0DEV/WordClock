0cd7735d37bc73b0e2293746e59908234159b29e
PA0DEV <91952522+PA0DEV@users.noreply.github.com>
2021-12-28 20:34:31 +0100

    added dht sensor

--- main.py
+++ main.py
@@ -10,11 +10,15 @@
 # 
 # version: 0.0.0
 # designed and tested on Wemos D1 mini (ESP8266)
+# external hardware: 
+#  - RTC module (DS3231)
+#  - Temperature and humidity sensor (DHT11)
 #
 # pin conenctions:
 #   2 -> builtin LED
 #   5 -> I2C clock
 #   4 -> I2C data
+#   0 -> DHT data
 # 
 # used external libaries:
 #  - ds3231.py
@@ -24,15 +28,20 @@ import network
 from machine import Pin, SoftI2C
 import ds3231
 import ntptime
+import dht
 from time import sleep, localtime
 
 # ----------------------------------------
+### setup rtc module ###
 timezone = 1
 wDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4), freq=100000)
 rtc = ds3231.DS3231(i2c)
 
+# ----------------------------------------
+### setup DHT11 module ###
+sensor = dht.DHT11(Pin(0))
 
 
 
@@ -47,5 +56,11 @@ while True:
     wday = wDays[time[6]]
 
     print("Current time: %s, %i.%i.%i ; %i:%i:%i"%(wday, day, month, year, hour, minute, second,))
+    sensor.mesure()
+    temp = sensor.temperature()
+    humid = sensor.humidity()
+
+    print("Temperature: %2.1f"%temp)
+    print("Humidity: %2.1f"%humid)
     
     sleep(1)
