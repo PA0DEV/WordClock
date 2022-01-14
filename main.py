6aaa5eea53909aa6f0711da4c6ff2fb2ac0901ff
PA0DEV <91952522+PA0DEV@users.noreply.github.com>
2022-01-14 11:13:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,13 @@ import ntptime
 import dht
 import uasyncio
 import json
-
+iyear = 0
+imonth = 1
+iday = 2
+ihour = 3
+iminute = 4
+isecond = 5
+iweekday = 6
 # ----------------------------------------
 ### setup rtc module ###
 
@@ -54,10 +60,90 @@ with open("info.json") as f:
 
 # ----------------------------------------
 ### ntp sync ###
-if info["device"]["online"] == True and settings["clock"]["autoTimeset"]:
+if info["device"]["online"] == True and settings["clock"]["autoTimeset"] == True:
     server = settings["clock"]["timeserver"]
     ntptime.settime(server)                 # get time from server
     rtc.save_time()                         # set RTC mudule time
+
+# ----------------------------------------
+### LED addresses ###
+# Segments
+#      __ __ __        __ __ __          __ __ __        12 13 14  
+#    __        __    __        __      __        __    11        15
+#    __        __    __        __      __        __    10        16
+#    __        __    __        __  42  __        __    _9        17
+#      __ __ __        __ __ __          __ __ __        20 19 18  
+#    __        65    __        44  43  __        21    _8        _0
+#    __        __    __        __      __        __    _7        _1
+#    __        __    __        __      __        __    _6        _2
+#       __ __ __       __ __ __           __ __ __       _5 _4 _3   
+
+## offset address for first segment LED
+segOffset = [105, 126, 149, 170]
+
+## address dot LEDs
+segDots = [147, 148]
+
+# address segment number / symbol
+segNumbers = [
+    [0, 1, 2, 3, 4, 5, 67, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],               # 0
+    [0, 1, 2, 15, 16, 17],                                                      # 1
+    [3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20],                     # 2
+    [0, 1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 20],                     # 3
+    [0, 1, 2, 9, 10, 11, 15, 16, 17, 18, 19, 20],                               # 4
+    [0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14, 18, 19, 20],                      # 5
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],             # 6
+    [0, 1, 2, 12, 13, 14, 15, 16, 17],                                          # 7
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], # 8
+    [0, 1, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],                   # 9
+    [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],                            # °
+    [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],                                  # C
+    [3, 7 , 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],                              # F
+    [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],                      # E
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 16, 17],                         # U
+    [6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],                               # P
+    [18, 19, 20]                                                                # -
+]
+## LED address prefix ##
+dispItIs = [0, 1, 2, 3, 4]
+
+## LED address hours ##
+dispHours = [
+    [83, 84, 85, 86, 87],       # 00 | 12
+    [56, 57, 58, 59],           # 01
+    [49, 50, 51, 52],           # 02 
+    [60, 61, 62, 63],           # 03
+    [64, 65, 66, 67],           # 04
+    [45, 46, 47, 48],           # 05
+    [72, 73, 74, 75, 76],       # 06
+    [77, 78, 79, 80, 81, 82],   # 07
+    [68, 69, 70, 71]            # 08
+    [41, 42, 43, 44]            # 09
+    [95, 96, 97, 98]            # 10
+    [53, 54, 55]                # 11
+]
+
+## LED address minutes ##
+dispMins = [
+    [], # 0                                                                # 00
+    [5, 6, 7, 8, 30, 31, 32, 33],                                          # 05
+    [19, 20, 21, 22, 30, 31, 32, 33],                                      # 10
+    [5, 6, 7, 8, 19, 20, 21, 22, 30, 31, 32, 33],                          # 15
+    [9, 10, 11, 12, 13, 14, 15, 30, 31, 32, 33],                           # 20
+    [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 30, 31, 32, 33],   # 25
+    [35, 36, 37, 38],                                                      # 30
+    [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 38, 39, 40],       # 35
+    [9, 10, 11, 12, 13, 14, 15,38, 39, 40],                                # 40
+    [5, 6, 7, 8, 19, 20, 21, 22, 38, 39, 40],                              # 45
+    [19, 20, 21, 22, 38, 39, 40],                                          # 50
+    [5, 6, 7, 8, 38, 39, 40],                                              # 55
+]
+
+## LED address morning / afternoon ##
+dispAmPm = [
+    [88, 89, 90, 91, 92, 93, 94],   # AM
+    [99, 100, 101, 102, 103, 104]   # PM
+]
 
 # ----------------------------------------
 ### setup async time and temperature update ###
@@ -73,21 +159,21 @@ async def getTimeAndTemp():
 
         utcTime = rtc.get_time()       # returns [year, month, day, hour, minute, second, wday, 0] (UTC time)
 
-        dstHour = utcTime[3] + timezone
+        dstHour = utcTime[ihour] + timezone
 
-        year = utcTime[0]
-        month = utcTime[1]
-        day = utcTime[2]
+        year = utcTime[iyear]
+        month = utcTime[imonth]
+        day = utcTime[iday]
         ## check for summer- wintertime
         if 4 < month < 9 or (month == 3 and (day > 27 or day == 27 and dstHour > 2) ) or (month == 10 and (day <30 or day == 30 and dstHour < 3)) :
             ## summertime from mar 27 2:00 to oct 30 3:00
-            hour = utcTime[3] + timezone +1
+            hour = utcTime[ihour] + timezone +1
         else :
-             hour = utcTime[3] + timezone
+             hour = utcTime[ihour] + timezone
         
-        minute = utcTime[4]
-        second = utcTime[5]
-        wDay = utcTime[6]
+        minute = utcTime[iminute]
+        second = utcTime[isecond]
+        wDay = utcTime[iweekday]
 
         print("Current time: %i.%i.%i ; %i:%i:%i"%(day, month, year, hour, minute, second,))
 
@@ -104,84 +190,61 @@ async def getTimeAndTemp():
         await uasyncio.sleep_ms(500)
 
 # ----------------------------------------
-###  ###
-async def display():
-    wDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    dispItIs = [0, 1, 2, 3, 4]
-    dispHours = [
-        [83, 84, 85, 86, 87],       # 00 | 12
-        [49, 50, 51, 52],           # 01
-        [56, 57, 58, 59],           # 02 
-        [60, 61, 62, 63],           # 03
-        [64, 65, 66, 67],           # 04
-        [45, 46, 47, 48],           # 05
-        [68, 69, 70, 71, 72],       # 06
-        [77, 78, 79, 80, 81, 82],   # 07
-        [73, 74, 75, 76]            # 08
-        [41, 42, 43, 44]            # 09
-        [88, 89, 90, 91]            # 10
-        [53, 54, 55]                # 11
-    ]
-    dispMins = [
-        [],                                                                             # 00
-        [5, 6, 7, 8, 37, 38, 39, 40],                                                   # 05
-        [19, 20, 21, 22, 37, 38, 39, 40],                                               # 10
-        [5, 6, 7, 8, 19, 20, 21, 22, 37, 38, 39, 40],                                   # 15
-        [12, 13, 14, 15, 16, 17, 18, 19, 20, 37, 38, 39, 40],                           # 20
-        [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 37, 38, 39, 40],    # 25
-        [33, 34, 35, 36]                                                                # 30
-        [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 31, 32],        # 35
-        [12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 31, 32],                               # 40
-        [5, 6, 7, 8, 19, 20, 21, 22, 30, 31, 32],                                       # 45
-        [19, 20, 21, 22, 30, 31, 32],                                                   # 50
-        [5, 6, 7, 8, 30, 31, 32],                                                       # 55
-    ]
+### write wordclock ###
+async def displayWords():
+    ### Words LED controll ### 
+    for adr in dispItIs:
+        print(adr)
+        ## LED CODE ##
+        ...
+    for adr in dispHours[locTime[ihour]]:
+        print(adr)
+        ## LED CODE ##
+        ...
+    for adr in dispMins[locTime[iminute] // 5]:
+        print(adr)
+        ## LED Code ##
+        ...
+    for adr in dispHours[locTime[ihour] // 12]:
+        print(adr)
+        ## LED Code ##
+        ...
 
-    #      __ __ __        __ __ __          __ __ __        12 13 14  
-    #    __        __    __        __      __        __    11        15
-    #    __        __    __        __      __        __    10        16
-    #    __        __    __        __  42  __        __    _9        17
-    #      __ __ __        __ __ __          __ __ __        20 19 18  
-    #    __        65    __        44  43  __        21    _8        _0
-    #    __        __    __        __      __        __    _7        _1
-    #    __        __    __        __      __        __    _6        _2
-    #       __ __ __       __ __ __           __ __ __       _5 _4 _3   
-    segNumbers = [
-        [0, 1, 2, 3, 4, 5, 67, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],               # 0
-        [0, 1, 2, 15, 16, 17],                                                      # 1
-        [3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20],                     # 2
-        [0, 1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 20],                     # 3
-        [0, 1, 2, 9, 10, 11, 15, 16, 17, 18, 19, 20],                               # 4
-        [0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14, 18, 19, 20],                      # 5
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],             # 6
-        [0, 1, 2, 12, 13, 14, 15, 16, 17],                                          # 7
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], # 8
-        [0, 1, 2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],                   # 9
-        [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],                            # °
-        [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],                                  # C
-        [3, 7 , 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],                              # F
-        [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],                      # E
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 16, 17],                         # U
-        [6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 19, 20],                               # P
-        [18, 19, 20]                                                                # -
-    ]
+    await uasyncio.sleep_ms(1000)
 
-    while True:
-        ### Words LED controll ### 
-        for adr in dispItIs:
-            print(adr)
-            ## LED CODE ##
-            ...
-        for adr in dispHours[locTime[3]]:
-            print(adr)
-            ## LED CODE ##
-            ...
-        for adr in dispMins[locTime[4]]:
-            print(adr)
-            ## LED Code ##
-            ...
+# ----------------------------------------
+### write clock on segments ###
+async def segmentClock():
+    ## Segment 0 - 1 minute ##
+    number = locTime[iminute] % 10
+    for adr in segNumbers[number]:
+        adr += segOffset[0]
+        ...
+    
+    ## Segment 1 - 10 minute ##
+    number = locTime[iminute] // 10
+    for adr in segNumbers[number]:
+        adr += segOffset[1]
+        ...
 
+    ## Segment 2 - 1 hour ##
+    number = locTime[ihour] % 10
+    for adr in segNumbers[number]:
+        adr += segOffset[2]
+        ...
 
+    ## Segment 3 - 10 hour
+    number = locTime[ihour] // 10
+    for adr in segNumbers[number]:
+        adr += segOffset[3]
+        ...
+    
+    ## dots ##
+    for adr in segDots:
+        ...
+    uasyncio.sleep(1000)
+    for adr in segDots:
+        ...
 
 # ----------------------------------------
 ### start the methods ###
